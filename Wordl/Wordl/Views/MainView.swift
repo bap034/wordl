cb4f01082faaa1118e3730de8e1dcdb9dc661bf8
Brett Petersen <bap034@gmail.com>
2021-12-17 22:46:34 -0800

    initial project creation

---
+++ Wordl/Wordl/Views/MainView.swift
@@ -0,0 +1,21 @@
+//
+//  MainView.swift
+//  Wordl
+//
+//  Created by Brett Petersen on 12/17/21.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct MainView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainView()
+    }
+}
---
+++ Wordl/Wordl/WordlApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordlApp.swift
+//  Wordl
+//
+//  Created by Brett Petersen on 12/17/21.
+//
+
+import SwiftUI
+
+@main
+struct WordlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainView()
+        }
+    }
+}
